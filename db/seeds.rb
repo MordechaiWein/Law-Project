1b909f31b80d463c326f9dce822ed55a6c2746cf
Mordechai <mordwein77@gmail.com>
2024-03-21 20:50:05 -0400

    added jwt secret key to render settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-puts 'Seeding Database...🌱🌱🌱'
+puts '🌱🌱🌱 Seeding database...'
 
-User.destroy_all
-User.create(name: "Martin Wein", password: "12345678%", password_confirmation: "12345678%", email: "mordwein77@gmail.com", boss: true, company_name: "Kick your ass legal group")
+puts "🔴"
+puts "🟡"
+puts "🟢"
+puts "🟣"
 
-puts 'Seeding complete!'
+puts '✅ Seeding complete!'
