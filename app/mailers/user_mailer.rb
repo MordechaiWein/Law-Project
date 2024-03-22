432a7f4ec55c554c2560010085275533e3bd1b8e
Mordechai <mordwein77@gmail.com>
2024-03-21 23:25:17 -0400

    why do i think this isnt going to work

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
     def sign_up_link_email
         # @url  = "http://localhost:4000/#{CGI.escape(params[:token])}"  
-        @url  = "https://law-project-d1k1.onrender.com/#{CGI.escape(params[:token])}"
+        @url  = "https://www.lawexample.com/#{params[:token]}"
         mail(to: params[:email], subject: 'Welcome to Law Automation - Complete Your Registration')
     end
     
