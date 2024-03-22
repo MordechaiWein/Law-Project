e3ed02ab35f0181ab9db599c216794b08bbf4659
Mordechai <mordwein77@gmail.com>
2024-03-21 20:09:23 -0400

    added smtp configuration to config environments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,21 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'law-project-d1k1.onrender.com',
+    user_name:            'lawautomationproject@gmail.com',
+    password:             'dvkc jfsz jdao pkfy',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
