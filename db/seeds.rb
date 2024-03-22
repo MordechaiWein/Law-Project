c7838341225f2bd2d815c5a493cae3956131507c
Mordechai <mordwein77@gmail.com>
2024-03-21 20:19:05 -0400

    switched url to send in email

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 puts 'Seeding Database...🌱🌱🌱'
 
-User.create(name: "Martin Wein", password: "12345678?", password_confirmation: "12345678?", email: "mordwein77@gmail.com", boss: true, company_name: "Kick your ass legal group")
+User.destroy_all
+User.create(name: "Martin Wein", password: "12345678%", password_confirmation: "12345678%", email: "mordwein77@gmail.com", boss: true, company_name: "Kick your ass legal group")
 
 puts 'Seeding complete!'
