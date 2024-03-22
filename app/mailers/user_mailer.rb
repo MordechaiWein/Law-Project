52de728810d2b5abdbee60bcdc75e5ededdd2053
Mordechai <mordwein77@gmail.com>
2024-03-21 21:07:19 -0400

    remvoed qutes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
 
     def sign_up_link_email
+        # @url  = "http://localhost:4000/#{params[:token]}"  
         @url  = "https://law-project-d1k1.onrender.com/#{params[:token]}"
         mail(to: params[:email], subject: 'Welcome to Law Automation - Complete Your Registration')
     end
