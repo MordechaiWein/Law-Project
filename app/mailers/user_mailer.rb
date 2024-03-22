b89333aa15bd9925c365ac4964bba03c9d2e25a1
Mordechai <mordwein77@gmail.com>
2024-03-21 21:43:42 -0400

    cgi escape attempt fix please work

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
 
     def sign_up_link_email
-        # @url  = "http://localhost:4000/#{params[:token]}"  
-        @url  = "https://law-project-d1k1.onrender.com/#{params[:token]}"
+        # @url  = "http://localhost:4000/#{CGI.escape(params[:token])}"  
+        @url  = "https://law-project-d1k1.onrender.com/#{CGI.escape(params[:token])}"
         mail(to: params[:email], subject: 'Welcome to Law Automation - Complete Your Registration')
     end
     
