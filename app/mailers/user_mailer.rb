cfad639f04b25996ed426b5715bc06daed7da380
Mordechai <mordwein77@gmail.com>
2024-03-24 01:19:34 -0400

    la la la fuck dragin just solved big problem I am a master programmer you cant hold me you cant stop me ill level you

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
 
     def sign_up_link_email
-        @url  = "http://localhost:4000/#{params[:token]}"  
-        # @url  = "https://www.lawexample.com/#{params[:token]}"
+        # @url  = "http://localhost:4000/#{params[:token]}"  
+        @url  = "https://www.lawexample.com/#{params[:token]}"
         mail(to: params[:email], subject: 'Welcome to Law Automation - Complete Your Registration')
     end
     
