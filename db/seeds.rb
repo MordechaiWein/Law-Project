22207685357bda1e10f16684b216e1015ba63a55
Mordechai <mordwein77@gmail.com>
2024-03-24 04:30:54 -0400

    gg

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
 puts '🌱🌱🌱 Seeding database...'
 
-User.destroy_all
-User.create(name: 'Martin Wein', password: 'fuckdragin1!', password_confirmation: 'fuckdragin1!', email: 'mordwein77@gmail.com', boss: true, company_name: 'fuck dragin')
 puts "🔴"
 puts "🟡"
 puts "🟢"
-puts "🟣"
 
 puts '✅ Seeding complete!'
