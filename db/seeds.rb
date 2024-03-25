90f95ff11a4d96f6d398f42308d8e8dfa322fc29
Mordechai <mordwein77@gmail.com>
2024-03-24 22:37:19 -0400

    new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts '🌱🌱🌱 Seeding database...'
 
+User.destroy_all
+User.create(name: 'Martin Wein', password: '12345678!' password_confirmation: '12345678!', email: 'mordwein77@gmail.com', boss: true, company_name: 'Velocity Capital Group')
 puts "🔴"
 puts "🟡"
 puts "🟢"
